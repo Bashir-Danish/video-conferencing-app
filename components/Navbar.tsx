c126f1859f4b1ebb3756a9fde3e9bca4394becfc
Bashir-Danish <bashirdanish124@gmail.com>
2024-05-08 17:43:17 +0430

    auth section completed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,7 +2,10 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import MobileNav from './MobileNav'
-
+import { SignIn, UserButton } from '@clerk/nextjs'
+import {
+  SignedIn,
+} from "@clerk/nextjs";
 const Navbar = () => {
   return (
     <nav className='flex flex-between fixed z-50 w-full bg-dark-1 px-6 py-7 lg:px-10'>
@@ -11,6 +14,10 @@ const Navbar = () => {
         <p className='text-[26px] font-extrabold text-white max-sm:hidden'>Loom</p>
       </Link>
       <div className=" flex-between gap-5">
+        <SignedIn>
+            <UserButton/>
+        </SignedIn>
+  
         <MobileNav/>
       </div>
     </nav>
